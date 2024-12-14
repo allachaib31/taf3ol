8bc307481dce35957abe9ae394251f7957da1723
allachaib54 <allachaib54@gmail.com>
2024-12-14 10:53:14 +0100

    fix configuration in helmet

--- server/server.js
+++ server/server.js
@@ -33,23 +33,26 @@ conn.once('open', () => {
     app
         .use(express.json({ limit: '50mb' }))
         .use(express.urlencoded({ limit: '50mb', extended: true }))
-        .use(helmet({
-            contentSecurityPolicy: {
-                useDefaults: true,
-                directives: {
-                    "default-src": ["'self'"],
-                    "script-src": ["'self'"],
-                    "style-src": ["'self'", "'unsafe-inline'"],
-                    "img-src": ["'self'", "data:"],
-                    "object-src": ["'none'"],
-                    "frame-ancestors": ["'self'"],
+        .use(
+            helmet({
+                contentSecurityPolicy: {
+                    useDefaults: true,
+                    directives: {
+                        "default-src": ["'self'"],
+                        "script-src": ["'self'"],
+                        "style-src": ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
+                        "font-src": ["'self'", "https://fonts.gstatic.com"],
+                        "img-src": ["'self'", "data:", "https://img.daisyui.com"],
+                        "object-src": ["'none'"],
+                        "frame-ancestors": ["'self'"],
+                    },
                 },
-            },
-            crossOriginEmbedderPolicy: true,
-            crossOriginOpenerPolicy: { policy: "same-origin" },
-            crossOriginResourcePolicy: { policy: "same-origin" },
-            referrerPolicy: { policy: "no-referrer" },
-        }))
+                crossOriginEmbedderPolicy: true,
+                crossOriginOpenerPolicy: { policy: "same-origin" },
+                crossOriginResourcePolicy: { policy: "same-origin" },
+                referrerPolicy: { policy: "no-referrer" },
+            })
+        )
         .use((req, res, next) => {
             res.setHeader("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
             next();
